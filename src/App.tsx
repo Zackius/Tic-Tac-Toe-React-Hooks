a61a7f645de05f084e7d8d1b7781c1ec88db2dd8
Zacharia <ndunguzachary24@gmail.com>
2023-05-23 23:32:13 +0300

    tic tac toe boxes with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import styled from "styled-components"
+import TicTacToe from "./Components/TicTacToe"
+import "papercss/dist/paper.min.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Main>
+      <TicTacToe />
+    </Main>
+  );
 }
-
+const Main = styled.main`
+display:flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 export default App
+
+
