b1855285a69e7822d13da9e74ed4e0b83ab9aa4e
Zacharia <ndunguzachary24@gmail.com>
2023-05-24 11:08:40 +0300

    game state, ai and human move on null at the start

--- src/Components/TicTacToe.tsx
+++ src/Components/TicTacToe.tsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react'
 import styled from "styled-components"
 import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMS } from '../constants'
+import { getRandomInt } from '../utils'
 
 const emptyGrid = new Array(DIMENSIONS ** 2).fill(null)
 
 const TicTacToe = () => {
     const [grid, setGrid] = useState(emptyGrid)
-    const [players, setPlayers] = useState({
-        human: PLAYER_X,
-        ai: PLAYER_O
+    const [players, setPlayers] = useState<Record<string, number | null >>({
+        human: null
+        ai: null
     });
 
-    const move = (index: number, player: number) => {
-        setGrid((grid) => {
-            const gridCopy = grid.concat()
-            gridCopy[index] = player;
-            return gridCopy
-        })
-        const humanMove = (index: number) => {
-            if (!grid[index]) {
-                 move(index, players.human)
-             }
+    const move = (index: number, player: number | null) => {
+        if (player !== null) {
+            
+            setGrid((grid) => {
+                const gridCopy = grid.concat()
+                gridCopy[index] = player;
+                return gridCopy
+            })
         }
-        
+   
     }
+          const aiMove = () => {
+            let index = getRandomInt(0, 8);
+            while (grid[index]) {
+                index = getRandomInt(0, 8)
+            }
+            move(index, players.ai)
+        }
+    const humanMove = (index: number) => {
+        if (!grid[index]) {
+            move(index, players.human)
+            aiMove();
+         }
+    }
+    
     return (
         <Container dims={DIMENSIONS}>
           {grid.map((value, index) => {
